286fdabe5bd8e20e8977d1ccd304bdfc5999d6a2
Ribeiro dos Santos Neto <intersect.2.0@hotmail.com>
2022-10-26 10:13:22 -0300

    Basic Security Configuration

---
+++ src/main/java/com/ecommer/backend/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.ecommer.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeRequests().anyRequest().permitAll()
+                .and().httpBasic().and().build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
