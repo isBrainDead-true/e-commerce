b7d4c1b77a34b3caec7d32e25ebe21a3d1dce633
Ribeiro dos Santos Neto <intersect.2.0@hotmail.com>
2022-10-27 10:57:15 -0300

    CORS configuration applied

--- src/main/java/com/ecommer/backend/security/WebSecurityConfig.java
+++ src/main/java/com/ecommer/backend/security/WebSecurityConfig.java
@@ -1,12 +1,24 @@
 package com.ecommer.backend.security;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -16,9 +28,11 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        //Resolving CORS
+        http.csrf().disable().cors();
+
         //Allow H2 Database Connection
         http.authorizeRequests().antMatchers("/h2-console/**");
-        http.csrf().disable();
         http.headers().frameOptions().disable();
 
         //Normal Filter Chain
@@ -34,4 +48,6 @@ public class WebSecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+
 }
