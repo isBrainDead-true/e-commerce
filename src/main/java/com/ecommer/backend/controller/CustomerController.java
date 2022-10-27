e955b2ff1aeeb42b99538d5aa795f4e54ba21e1e
Ribeiro dos Santos Neto <intersect.2.0@hotmail.com>
2022-10-27 10:57:57 -0300

    @CrossOrigin

--- src/main/java/com/ecommer/backend/controller/CustomerController.java
+++ src/main/java/com/ecommer/backend/controller/CustomerController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("api/v1/customer")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class CustomerController {
 
     private final CustomerService service;
